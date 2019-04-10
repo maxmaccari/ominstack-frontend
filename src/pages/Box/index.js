81bc60027c1d976ea27d3a9593e4ecdfdfcb0270
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-10 17:37:13 -0400

    Create Box component

---
+++ src/pages/Box/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+
+export default class Box extends Component {
+  render() {
+    console.log(this.props)
+    return <div />;
+  }
+}
