be2c431928456f1cfcc5b1125f8417b2f6cd770a
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-10 17:44:51 -0400

    Mark and style Box component

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 
+import { MdInsertDriveFile } from 'react-icons/md'
+
+import logo from "../../assets/logo.svg"
 import './styles.css';
 
 export default class Box extends Component {
   render() {
-    console.log(this.props)
-    return <div />;
+    return (
+      <div id="box-container">
+      <header>
+        <img src={logo} alt="RocketBox"/>
+        <h1>Rocketseat</h1>
+      </header>
+
+      <ul>
+        <li>
+          <a className="fileInfo" href="">
+            <MdInsertDriveFile size={24} color="#a5cfff" />
+            <strong>Desafio.pdf</strong>
+          </a>
+
+          <span>há 3 minutos atrás</span>
+        </li>
+        <li>
+          <a className="fileInfo" href="">
+            <MdInsertDriveFile size={24} color="#a5cfff" />
+            <strong>Desafio.pdf</strong>
+          </a>
+
+          <span>há 3 minutos atrás</span>
+        </li>
+      </ul>
+    </div>
+    );
   }
 }
