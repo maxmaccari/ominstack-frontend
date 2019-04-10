d9fd5bb3b3afe07674e9221ae553d754595d3f5a
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-10 12:34:46 -0400

    Implement box creation by api call
    https://www.youtube.com/watch?v=ua9nKS3XYpw

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
+import api from '../../services/api'
 
 import logo from '../../assets/logo.svg'
 import './styles.css';
 
 export default class Main extends Component {
+  state = {
+    newBox: '',
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const response = await api.post('boxes', {
+      title: this.state.newBox,
+    });
+
+    console.log(response.data);
+
+    this.setState({
+      newBox: ''
+    });
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      newBox: e.target.value
+    });
+  }
+
   render() {
     return <div id="main-container">
-      <form action="">
-        <img src={logo} alt=""/>
-        <input placeholder="Criar um box" />
+      <form onSubmit={this.handleSubmit} action="">
+        <img src={logo} alt="RocketBox"/>
+        <input
+          placeholder="Criar um box"
+          value={this.state.newBox}
+          onChange={this.handleInputChange}
+        />
         <button type="submit">Criar</button>
       </form>
     </div>;
